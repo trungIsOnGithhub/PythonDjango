a5088ac35b0febe45691f770a9cb05955b2841f5
trung <trung150502@gmail.com>
2024-01-16 12:41:20 +0700

    db config file

---
+++ crm/db.py
@@ -0,0 +1,21 @@
+# https://dev.mysql.com/downloads/installer/
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python 
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+	passwd = 'password123'
+
+	)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("DB Done!")
