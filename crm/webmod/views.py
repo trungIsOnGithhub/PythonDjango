96690e638496b3f3b0fefae4bd50ee92c3a06c6c
trung <trung150502@gmail.com>
2024-01-16 13:51:00 +0700

    home views

--- crm/webmod/views.py
+++ crm/webmod/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
 # Create your views here.
+def home(request):
+
+	records = Record.objects.all()
+
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+
+			login(request, user)
+
+			messages.success(request, "You Have Been Logged In!")
+
+			return redirect('home')
+		else:
+			messages.success(request, "There Was An Error Logging In, Please Try Again...")
+
+			return redirect('home')
+	else:
+		return render(request, 'home.html', {'records':records})
+
+
+def logout_user(request):
+	logout(request)
+
+	messages.success(request, "You Have Been Logged Out...")
+
+	return redirect('home')
+
+
+def register_user(request):
+	form = SignUpForm()
+
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+
+			user = authenticate(username=username, password=password)
+
+			login(request, user)
+
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+
+			return redirect('home')
+	else:
+		return render(request, 'register.html', {'form' : form})
+
+	return render(request, 'register.html', {'form' : form})
