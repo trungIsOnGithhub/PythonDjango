755a09552a50400560fcdd8e938261370ff003cf
trung <trung150502@gmail.com>
2024-01-16 12:24:45 +0700

    delete old files

--- app/proj/kanban/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from car.models import Car
-
-class CarSerializer(serializers.ModelSerializer):
-    class Meta: 
-        model = Car
-        fields = ('name', 'color', 'brand')
--- app/proj/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('cars', views.ListCreateCarView.as_view()),
-    path('cars/<int:pk>', views.UpdateDeleteCarView.as_view()),
-]
