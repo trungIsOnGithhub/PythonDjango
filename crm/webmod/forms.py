ae86052e260c3920239f612ae9a27d3a91a46bee
trung <trung150502@gmail.com>
2024-01-16 13:07:56 +0700

    update add record form

--- crm/webmod/forms.py
+++ crm/webmod/forms.py
@@ -9,3 +9,17 @@ class SignUpForm(UserCreationForm):
 
 	def __init__(self, *args, **kwargs):
 		super(SignUpForm, self).__init__(*args, **kwargs)
+
+class AddRecordForm(forms.ModelForm):
+	first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"class":"form-control", "placeholder":"First Name"}), label="")
+	last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"class":"form-control", "placeholder":"Last Name"}), label="")
+	email = forms.EmailField(required=True, label="", widget=forms.TextInput(attrs={"class":"form-control", 'placeholder':'Email Address'}))
+	phone = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"class":"form-control", "placeholder":"Phone"}), label="")
+	address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"class":"form-control", "placeholder":"Address"}), label="")
+	city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"class":"form-control", "placeholder":"City"}), label="")
+	state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"class":"form-control", "placeholder":"State"}), label="")
+	zipcode = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"class":"form-control", "placeholder":"Zipcode"}), label="")
+
+	class Meta:
+		model = Record
+		exclude = ("user")
