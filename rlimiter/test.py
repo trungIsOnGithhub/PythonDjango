0dfb0dff674186efb2ea2fafe11a6ce16983c0aa
Trung Nguyen <trung150502@gmail.com>
2024-06-12 14:54:59 +0000

    fix tests

--- rlimiter/test.py
+++ rlimiter/test.py
@@ -1,5 +1,29 @@
 
-from tests.test_lock_service import test_lock_service
+# from tests.test_lock_service import test_lock_service
 
+# if __name__ == '__main__':
+#     test_lock_service()
+
+from main.lock_service import *
+# import unittest
+
+class TestDataSource(ClientDataSource):
+    def find_all(self) -> list[ClientData]:
+        return [ ClientData(client_id='client-1'), ClienData(client_id='client-2'), ClientData('client-3') ] # temporarily hard coded data
+
+# class TestLockService(unittest.TestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         self.data_source = TestDataSource()
+#         self.lock_service = ClientLockService(data_source=data_source)
+
+#     def test_
 if __name__ == '__main__':
-    test_lock_service()
+    data_src = TestDataSource()
+    lock_service = ClientLockService(data_source=data_src)
+
+    lock_service.initialize_client_locks()
+    lock_service.add_client('client-69')
+
+    print(lock_service.get_and_validate_client_lock('client-1'))
+    print(lock_service.get_and_validate_client_lock('client-69'))
--- rlimiter/tests/test_lock_service.py
+++ rlimiter/tests/test_lock_service.py
@@ -1,11 +1,23 @@
 from rlimiter.main.lock_service import *
+# import unittest
 
 class TestDataSource(ClientDataSource):
     def find_all(self) -> list[ClientData]:
         return [ ClientData(client_id='client-1'), ClienData(client_id='client-2'), ClientData('client-3') ] # temporarily hard coded data
 
-def test_lock_service():
-    data_source = TestDataSource()
-    lock_service = ClientLockService(data_source=data_source)
+# class TestLockService(unittest.TestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         self.data_source = TestDataSource()
+#         self.lock_service = ClientLockService(data_source=data_source)
 
-    
+#     def test_
+if __name__ == '__main__':
+    data_src = TestDataSource()
+    lock_service = ClientLockService(data_source=data_src)
+
+    lock_service.initialize_client_locks()
+    lock_service.add_client('client-69')
+
+    print(lock_service.get_and_validate_client_lock('client-1'))
+    print(lock_service.get_and_validate_client_lock('client-69'))
